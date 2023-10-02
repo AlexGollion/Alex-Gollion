17b969cba256ff769d713531f2ab8cb513098805
Alex Gollion <alex.gollion@gmail.com>
2023-10-02 11:00:56 +0200

    second commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './Pages/Home';
